e5900f35a55417b315ca0cb8b5cbdac414f29016
jaketrent <trent.jake@gmail.com>
2012-10-13 16:10:58 -0600

    add site icon

--- wp-content/themes/living-journal/header.php
+++ wp-content/themes/living-journal/header.php
@@ -17,8 +17,11 @@
 	<header id="branding" role="banner">
       <div id="inner-header" class="clearfix">
 		<hgroup id="site-heading">
-			<h1 id="site-title"><a href="<?php echo home_url( '/' ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<h2 id="site-description"><?php bloginfo( 'description' ); ?></h2>
+            <img src="/wp-content/themes/living-journal/library/images/jt2.png" id="site-img" />
+            <div id="site-verbage">
+                <h1 id="site-title"><a href="<?php echo home_url( '/' ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+                <h2 id="site-description"><?php bloginfo( 'description' ); ?></h2>
+            </div>
 		</hgroup>
 
 		<nav id="access" role="navigation">
